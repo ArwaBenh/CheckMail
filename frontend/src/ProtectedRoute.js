795bfc7c8c7cb0273eb51157188a6b8203693926
Arwa Ben Hamida <115593205+ArwaBenh@users.noreply.github.com>
2026-04-24 13:57:44 +0100

    Delete frontend directory

--- frontend/src/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-// ── ProtectedRoute.js ─────────────────────────────────────────────────────
-// Redirige vers /login si l'utilisateur n'est pas connecté
-
-import { Navigate } from "react-router-dom";
-import { isLogged } from "./authService";
-
-export default function ProtectedRoute({ children }) {
-  return isLogged() ? children : <Navigate to="/login" replace />;
-}
